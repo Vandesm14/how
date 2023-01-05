a4cb1fcaff06ae52debedeb16a1590d566d6859f
Shane Vandegrift <vandesm14@gmail.com>
2023-01-05 13:34:21 -0500

    move personal tasks to local build script

--- build.sh
+++ build.sh
@@ -5,9 +5,3 @@ deno compile --allow-net --allow-read --allow-write --allow-env --allow-run mod.
 # Move binary to bin folder
 mkdir -p bin
 mv how bin/how
-
-# Remove existing symlink
-rm /home/shane/apps/bin/how
-
-# Create symlink
-ln -s /home/shane/dev/how/bin/how /home/shane/apps/bin/how
