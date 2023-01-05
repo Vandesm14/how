bdb995e7ccd7d5bcb843368c465e64b08b16caed
Shane Vandegrift <vandesm14@gmail.com>
2023-01-05 17:00:39 -0500

    new line :)

--- build.sh
+++ build.sh
@@ -2,5 +2,6 @@
 
 # create a `bin` directory
 mkdir -p ./bin
+
 # compile deno programme into the `bin` directory
 deno compile --check=all --allow-net --allow-read --allow-write --allow-env --allow-run -o=./bin/how mod.ts
